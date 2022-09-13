ce7cb076a3a2df1f6cbc0cdcdf1f757794584cc3
MobScripts <113401404+MobScripts@users.noreply.github.com>
2022-09-12 21:14:56 -0600

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'V3CT0RBUG'
+description 'Boss utilities by V3CT0RBUG'
+version '1.0'
+
+server_script 'main.lua'
+
+boss_updater 'yes'
+boss_name '^2Mob Boss utilities'
+boss_github 'https://github.com/MobScripts/mob_bossutilities'
+
